63c930d8aa113167928ae05454140f1340a575cd
agrass-github <992951674@qq.com>
2019-10-09 13:13:11 +0800

    修复插槽不存在导致的报错

--- src/lib/agel-column.vue
+++ src/lib/agel-column.vue
@@ -1,22 +1,23 @@
 <template>
   <el-table-column v-bind="column" :key="column._key">
-    <template v-if="column.children&&column.children.length>0">
-      <agel-column v-for="item in column.children" :key="item.key" :column="item"></agel-column>
-    </template>
-
-    <template v-if="column.slotHeader" v-slot:header="scope">
+    <template v-if="isSlot(column.slotHeader)" v-slot:header="scope">
       {{renderSlot(column.slotHeader,scope)}}
       <slot :name="column.slotHeader"></slot>
     </template>
 
-    <template v-if="column.slotColumn" v-slot="scope">
+    <template v-if="isSlot(column.slotColumn)" v-slot="scope">
       {{renderSlot(column.slotColumn,scope)}}
       <slot :name="column.slotColumn"></slot>
     </template>
 
-    <template v-else-if="column.type=='expand'" v-slot="scope">
+    <!-- expand need v-else -->
+    <template v-else-if="isSlot(column.type)&&column.type=='expand'" v-slot="scope">
       {{renderSlot('expand',scope)}}
       <slot name="expand"></slot>
+    </template>
+
+    <template v-if="column.children&&column.children.length>0">
+      <agel-column v-for="item in column.children" :key="item.key" :column="item"></agel-column>
     </template>
   </el-table-column>
 </template>
@@ -32,8 +33,11 @@ export default {
     }
   },
   methods: {
-    renderSlot(key, scope) {
-      this.$slots[key] = this.table.$scopedSlots[key]({ ...scope });
+    isSlot(slotName) {
+      return slotName && this.table.$scopedSlots[slotName];
+    },
+    renderSlot(slotName, scope) {
+      this.$slots[slotName] = this.table.$scopedSlots[slotName]({ ...scope });
     }
   }
 };
